592b84c2c87f765086002344329bec3b7b9a63f3
tpaskett <tpaskett@paskettfamily.com>
2016-10-11 14:27:48 -0600

    v1.0

--- src/shared/data/www/cgi-bin/meshchatlib.pm
+++ src/shared/data/www/cgi-bin/meshchatlib.pm
@@ -2,7 +2,7 @@ BEGIN { push @INC, '/www/cgi-bin', '/usr/lib/cgi-bin' }
 
 use meshchatconfig;
 
-our $version = '0.7b7';
+our $version = '1.0';
 
 $messages_db_file = $messages_db_file_orig . '.' . zone_name();
 
